14dd27e97b1359f692381e7ad90012b656fb5f38
Job <contact@fontys.me>
2018-12-07 00:02:36 +0100

    Progress bar works as intended and file preview and internal file list are wiped after upload

--- src/app/upload-picture/upload-picture.component.ts
+++ src/app/upload-picture/upload-picture.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { PictureService } from '../services/picture.service';
+import { HttpUploadProgressEvent } from '@angular/common/http/src/response';
+
 
 @Component({
   selector: 'app-upload-picture',
@@ -29,14 +31,25 @@ export class UploadPictureComponent implements OnInit {
       });
   }
 
-  uploadImages() {
+  async uploadImages() {
+    // Upload picture and save progress to observable
     const uploadProgress = this.pictureService.upload(this.selectedFiles);
 
+    // Create snackbar with observable for progress bar
     this.snackBar.openFromComponent(UploadProgressComponent, {
       data: { uploadProgress },
     });
+
+    // Wait for uploading to be finished and then clear selected files and preview URLs
+    uploadProgress.subscribe((event: HttpUploadProgressEvent) => {
+      if (event.loaded === event.total && event.loaded !== undefined) {
+        this.selectedFiles = null;
+        this.previewUrls = [];
+      }
+    });
   }
 
+  // Convert file into Base64 string
   preview(file): string {
     const mimeType = file.type;
     if (mimeType.match(/image\/*/) == null) {
@@ -46,14 +59,17 @@ export class UploadPictureComponent implements OnInit {
     // tslint:disable-next-line:prefer-const
     let reader = new FileReader();
 
+    // Activate reader
     reader.readAsDataURL(file);
+
     reader.onload = () => {
+      // Put strings into array
       this.previewUrls.push(reader.result.toString());
     };
   }
 }
 
-import { MAT_SNACK_BAR_DATA, MatSnackBar } from '@angular/material';
+import { MAT_SNACK_BAR_DATA, MatSnackBar, MatSnackBarRef } from '@angular/material';
 import { map } from 'rxjs/operators';
 
 @Component({
@@ -64,17 +80,33 @@ import { map } from 'rxjs/operators';
   styles: [`mat-progress-bar { margin-top: 5px;}`],
 })
 export class UploadProgressComponent {
-  constructor(@Inject(MAT_SNACK_BAR_DATA) public data) { }
+  constructor(
+    @Inject(MAT_SNACK_BAR_DATA) public data,
+    private _snackRef: MatSnackBarRef<UploadProgressComponent>,
+    ) { }
 
   private started = false;
+
+  // Save progress percentage as variable
   public progress = this.data.uploadProgress.pipe(
     map(({ loaded, total }) => {
-      if (loaded === undefined) {
-        return !this.started ? 0 : 100;
+
+      // Chronological if statement
+      if (loaded === undefined && !this.started) {
+        // Uploading hasn't started so return 0 progress
+        return 0;
       // tslint:disable-next-line:no-else-after-return
-      } else {
+      } else if (loaded !== undefined) {
+        // Uploading has started
         this.started = true;
+
+        // Convert progress to percentage and then to an integer
         return Math.round(loaded / (total || loaded) * 100);
+      } else {
+
+        // Uploading has finished
+        // Destroying element
+        this._snackRef.dismiss();
       }
     },
   ));
