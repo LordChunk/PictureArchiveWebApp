577d71c825428e9d02bc580aad5f5cc7a24f3201
Job <contact@chu.mk>
2020-04-26 18:03:11 +0200

    Added firebase to production builds as well

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
-  production: true,
+  production: false,
   apiUri: 'https://picturearchive1.azurewebsites.net',
+  firebase: {
+    apiKey: 'AIzaSyAvK_cpvCf6_zcr6YEWNZoBPECNp1_nKEw',
+    authDomain: 'images-chu-mk.firebaseapp.com',
+    databaseURL: 'https://images-chu-mk.firebaseio.com',
+    projectId: 'images-chu-mk',
+    storageBucket: 'images-chu-mk.appspot.com',
+    messagingSenderId: '26394416274',
+    appId: '1:26394416274:web:bff9348aac857dfafb024b',
+  },
 };
