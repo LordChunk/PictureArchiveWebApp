977e87f448cdec45818dc1a604eb848d4ac60f4d
Job <contact@chu.mk>
2020-04-27 13:45:30 +0200

    Fixed login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { AuthService } from '../services/authentication.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -15,12 +16,15 @@ export class LoginComponent implements OnInit {
     ) { }
 
   credentialsForm: FormGroup;
+  user: Observable<any>;
 
   ngOnInit() {
     this.credentialsForm = this.formBuilder.group({
       email: [{ value: 'something@mail.com', disabled: false }],
       password: [{ value: 'sexyAnd@securePass12', disabled: false }],
     });
+
+    this.user = this.authService.user;
   }
 
   login() {
