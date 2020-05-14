16acbfae144552683a78d21d09d43c9eaf5dcd2e
Job <contact@chu.mk>
2020-05-15 00:05:16 +0200

    Fixed production value in environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   apiUri: 'https://picturearchive1.azurewebsites.net',
   firebase: {
     apiKey: 'AIzaSyAvK_cpvCf6_zcr6YEWNZoBPECNp1_nKEw',
