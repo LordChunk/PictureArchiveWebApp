3820f1a05ceaf49cada17e73923a188049c1c8df
Job <contact@chu.mk>
2020-05-14 16:13:28 +0200

    Added picture mockdata and removed formbuilder stub

--- src/app/upload-picture/picture-form/picture-form.component.spec.ts
+++ src/app/upload-picture/picture-form/picture-form.component.spec.ts
@@ -1,25 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { MatChipInputEvent } from '@angular/material/chips';
 import { PictureFormComponent } from './picture-form.component';
 import { ENTER, COMMA } from '@angular/cdk/keycodes';
 describe('PictureFormComponent', () => {
   let component: PictureFormComponent;
   let fixture: ComponentFixture<PictureFormComponent>;
   beforeEach(() => {
-    const formBuilderStub = () => ({ group: object => ({}) });
-    const matChipInputEventStub = () => ({ input: { value: {} }, value: {} });
     TestBed.configureTestingModule({
       schemas: [NO_ERRORS_SCHEMA],
       declarations: [PictureFormComponent],
       providers: [
-        { provide: FormBuilder, useFactory: formBuilderStub },
-        // { provide: MatChipInputEvent, useFactory: matChipInputEventStub },
+        { provide: FormBuilder },
       ],
     });
     fixture = TestBed.createComponent(PictureFormComponent);
     component = fixture.componentInstance;
+
+    component.picture = {
+      base64: '',
+      dateUploaded: new Date('2020-04-29T16:03:20.475Z'),
+      dateTaken: new Date('2017-06-10T12:28:38Z'),
+      fileType: 'image/jpeg',
+      index: 1,
+      metaTags: ['none', 'and some'],
+      name: 'test name',
+    };
   });
   it('can load instance', () => {
     expect(component).toBeTruthy();
